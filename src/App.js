a0d72f31c6e241f1e8e3db6d53739d9b2d32f1b9
OROJAXON <kevfrank21@gmail.com>
2024-10-20 19:12:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './index.css';
 import Spinner from './Spinner';
 import ConfettiExplosion from 'react-confetti-explosion';
 
-const names = ['jesusfrk', 'bray', 'momo', 'eze', 'llmp3ll', 'tanner', 'whytheylovejj', 'aj', '8ph', 'king', 'crimson', 'travin', ]
-const predeterminedWinner = 'travin';
+const names = ['jesusfrk', 'bray', 'momo', 'eze', 'llmp3ll', 'tanner', 'whytheylovejj', 'aj', '8ph', 'king', 'crimson', 'bsk_tjay', ]
+const predeterminedWinner = 'whytheylovejj';
 
 const WinnerModal = ({ isOpen, onClose, winner }) => {
   if (!isOpen) return null;
